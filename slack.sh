4b37132abfdb6c858cc56f305f548ffaf481cef3
Simon Schmid <mail@sschmid.com>
2021-10-27 17:33:26 +0200

    Migrate to using secrets

--- slack.sh
+++ slack.sh
@@ -2,26 +2,26 @@
 
 slack::_new() {
   echo '# slack'
-  echo '# Potentially sensitive data. Do not commit.
-SLACK_WEBHOOK_URL="https://hooks.slack.com/services/abc123"
-SLACK_AUTH_TOKEN="xoxa-xxxxxxxxx-xxxx"'
+  echo '# secrets:
+# slack.webhook
+# slack.token'
 }
 
 slack::message_webhook() {
-  local message="$1"
+  local message="$1" webhook
+  webhook="$(bee::secrets slack.webhook)"
   curl -s -X POST \
     -H 'Content-type: application/json' \
     --data "{\"text\":\"${message}\"}" \
-    "${SLACK_WEBHOOK_URL}"
+    "${webhook}"
 
   echo
 }
 
 slack::message() {
-  local channel="$1"
-  local message="$2"
-  local parent_ts="${3:-}"
-  local json=$(cat <<EOF
+  local channel="$1" message="$2" parent_ts="${3:-}" json token
+  token="$(bee::secrets slack.token)"
+  json=$(cat <<EOF
 {
   "channel": "${channel}",
   "thread_ts": "${parent_ts}",
@@ -31,7 +31,7 @@ EOF
 )
   curl -s -X POST \
     -H 'Content-type: application/json' \
-    -H "Authorization: Bearer ${SLACK_AUTH_TOKEN}" \
+    -H "Authorization: Bearer ${token}" \
     --data "$json" \
     https://slack.com/api/chat.postMessage
 
@@ -39,11 +39,9 @@ EOF
 }
 
 slack::upload() {
-  local channels="$1"
-  local message="$2"
-  local file="$3"
-  local parent_ts="${4:-}"
-  local json=$(cat <<EOF
+  local channels="$1" message="$2" file="$3" parent_ts="${4:-}" json token
+  token="$(bee::secrets slack.token)"
+  json=$(cat <<EOF
 {
   "file": "@${file}",
   "thread_ts": "${parent_ts}",
@@ -53,7 +51,7 @@ slack::upload() {
 EOF
 )
   curl -s \
-    -H "Authorization: Bearer ${SLACK_AUTH_TOKEN}" \
+    -H "Authorization: Bearer ${token}" \
     -F "thread_ts=${parent_ts}" \
     -F "initial_comment=${message}" \
     -F "channels=${channels}" \
