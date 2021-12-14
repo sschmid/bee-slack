d7ec240a67711caf2da87f343b835159b46101a1
Simon Schmid <mail@sschmid.com>
2021-12-14 22:17:27 +0100

    Update plugin according to bee 1.0.0 specs

--- slack.bash
+++ slack.bash
@@ -1,37 +1,55 @@
-#!/usr/bin/env bash
+slack::help() {
+  cat << 'EOF'
+Send messages via slack - https://github.com/sschmid/bee-slack
 
-slack::_new() {
-  echo '# slack'
-  echo '# secrets:
-# slack.webhook
-# slack.token'
+secrets:
+
+  SLACK_WEBHOOK
+  SLACK_TOKEN
+
+usage:
+
+  message_webhook                                           send a message using incoming webhooks
+                                                            e.g. bee slack message_webhook "Build complete"
+  message <channel> <message> [<parent-timestamp>]          send a message using https://slack.com/api/chat.postMessage
+                                                            e.g. bee slack message ABCD12345 "Build complete"
+  upload <channels> <message> <file> [<parent-timestamp]>   send a message and upload a file using https://slack.com/api/files.upload
+
+bee dependencies:
+
+  none
+
+dependencies:
+
+  slack   https://slack.com
+  curl
+
+EOF
 }
 
 slack::message_webhook() {
-  local message="$1" webhook
-  webhook="$(bee::secrets slack.webhook)"
+  local message="$1"
   curl -s -X POST \
     -H 'Content-type: application/json' \
     --data "{\"text\":\"${message}\"}" \
-    "${webhook}"
+    "${SLACK_WEBHOOK}"
 
   echo
 }
 
 slack::message() {
-  local channel="$1" message="$2" parent_ts="${3:-}" json token
-  token="$(bee::secrets slack.token)"
-  json=$(cat <<EOF
+  local channel="$1" message="$2" parent_ts="${3:-}" json
+  json=$(cat << EOF
 {
   "channel": "${channel}",
   "thread_ts": "${parent_ts}",
   "text": "${message}"
 }
 EOF
-)
+  )
   curl -s -X POST \
     -H 'Content-type: application/json' \
-    -H "Authorization: Bearer ${token}" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
     --data "$json" \
     https://slack.com/api/chat.postMessage
 
@@ -39,9 +57,8 @@ EOF
 }
 
 slack::upload() {
-  local channels="$1" message="$2" file="$3" parent_ts="${4:-}" json token
-  token="$(bee::secrets slack.token)"
-  json=$(cat <<EOF
+  local channels="$1" message="$2" file="$3" parent_ts="${4:-}" json
+  json=$(cat << EOF
 {
   "file": "@${file}",
   "thread_ts": "${parent_ts}",
@@ -49,9 +66,9 @@ slack::upload() {
   "channels": "${channels}"
 }
 EOF
-)
+  )
   curl -s \
-    -H "Authorization: Bearer ${token}" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
     -F "thread_ts=${parent_ts}" \
     -F "initial_comment=${message}" \
     -F "channels=${channels}" \
